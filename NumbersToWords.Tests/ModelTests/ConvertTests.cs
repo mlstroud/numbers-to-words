955792df8f1a9a0b5c216b4d4fef7dddc8e0218c
Matt Stroud <lixingke06@gmail.com>
2020-05-14 14:51:30 -0700

    add constructor test for storing digits in a queue

--- NumbersToWords.Tests/ModelTests/ConvertTests.cs
+++ NumbersToWords.Tests/ModelTests/ConvertTests.cs
@@ -1,11 +1,25 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NumbersToWords.Models;
+using System.Collections.Generic;
 
 namespace NumbersToWords.Tests
 {
   [TestClass]
   public class ConvertNumberTests
   {
+    [TestMethod]
+    public void Constructor_StoresDigitsInQueue_IntQueue()
+    {
+      string userInput = "123";
+      ConvertNumber newNum = new ConvertNumber(userInput);
+      Queue<int> queue = new Queue<int>();
+      queue.Enqueue(1);
+      queue.Enqueue(2);
+      queue.Enqueue(3);
 
+      Queue<int> result = newNum.GetQueue();
+
+      CollectionAssert.AreEqual(result, queue);
+    }
   }
 }
