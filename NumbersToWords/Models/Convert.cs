97b3ad35b5a7ba350087c2a5de8bb19f09645977
Matt Stroud <lixingke06@gmail.com>
2020-05-14 14:54:41 -0700

    update constructor to store digits in queue

--- NumbersToWords/Models/Convert.cs
+++ NumbersToWords/Models/Convert.cs
@@ -1,7 +1,28 @@
+using System.Collections.Generic;
+
 namespace NumbersToWords.Models
 {
   public class ConvertNumber
   {
+    private Queue<int> _digits;
+    private List<string> _ones;
+    private List<string> _tens;
+    private List<string> _teens;
 
+    public ConvertNumber(string inputNumber)
+    {
+      char[] letters = inputNumber.ToCharArray();
+      _digits = new Queue<int>();
+
+      foreach (char letter in letters)
+      {
+        _digits.Enqueue((int)(char.GetNumericValue(letter)));
+      }
+    }
+
+    public Queue<int> GetQueue()
+    {
+      return _digits;
+    }
   }
 }
